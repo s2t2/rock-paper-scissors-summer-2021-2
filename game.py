f6b7bd64da5eaaabf7f11f296d46077fd37c78b0
MJ Rossetti <s2t2@users.noreply.github.com>
2021-07-08 20:59:47 -0400

    Generate computer choice

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 # game.py
 
+import random
+
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 #
@@ -24,9 +28,26 @@ else:
     print("OOPS, INVALID, PLEASE TRY AGAIN")
     exit()
 
-print("LATER MESSAGES")
+#print("LATER MESSAGES")
+
+print("USER CHOSE: ", x)
 
 # GENERATE A COMPUTER CHOICE
+# https://docs.python.org/3/library/random.html
+# source: https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
+#
+# import random
+#
+# foo = ['a', 'b', 'c', 'd', 'e']
+# print(random.choice(foo))
+
+# more about datatypes lists, and tuples, etc next class
+#valid_options = ("rock", "paper", "scissors") # tuple
+valid_options = ["rock", "paper", "scissors"] # list
+
+c = random.choice(valid_options)
+
+print("COMPUTER CHOSE:", c)
 
 
 # DETERMINE THE WINNER
