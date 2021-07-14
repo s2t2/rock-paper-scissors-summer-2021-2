5f54ff132b5e61fdd79a86138ef2f300a97f2d9c
MJ Rossetti <s2t2@users.noreply.github.com>
2021-07-13 22:46:09 -0400

    Desire to refactor
    It just feels good / right to do

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # rock-paper-scissors-summer-2021-2
-a game of rock, paper, scissors
+
+A game of rock, paper, scissors!
+
+## Prerequisites
+
+You should have installed Python version 3.8+.
+
+## Setup
+
+Clone this repo onto your local machine, then navigate to it from the command-line:
+
+```sh
+cd /path/to/rock-paper-scissors-summer-2021-2
+```
+
+Setup / activate a virtual environment:
+
+```sh
+conda create -n my-game-env python=3.8
+conda activate my-game-env
+```
+
+Install package dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+Create a file called ".env" in the root directory of the local repo, and place inside contents like the following (to customize the player name):
+
+```sh
+USER_NAME="Your Name"
+```
+
+## Usage
+
+Run the game:
+
+```sh
+python game.py
+```
--- game.py
+++ game.py
@@ -1,101 +1,49 @@
-# game.py
+# this is the game.py file!
 
 import random
 import os
+
 from dotenv import load_dotenv
 
-load_dotenv() #> loads contents of the .env file into the script's environment
+load_dotenv() #> loads contents of the .env file into the environment
 
-z = os.getenv("USER_NAME")
-
-print(z) # reads the variable from the environment
-#> "Hello World"
-
-
-
-#exit()
-
-
-
-
-
-
-
+USER_NAME = os.getenv("USER_NAME") # reads the variable from the environment
 
 print("Rock, Paper, Scissors, Shoot!")
+print("WELCOME", USER_NAME)
 
 #
 # ASK FOR A USER INPUT
-# source: https://docs.python.org/3/library/functions.html#input
-#s = input('--> ')
+# see: https://docs.python.org/3/library/functions.html#input
+#
 
-x = input("Please choose one of 'rock', 'paper', 'scissors': ")
-print(x)
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
+user_choice = user_choice.lower()
+print("USER CHOSE:", user_choice)
 
-x = x.lower()
-print(x)
-
-
+#
 # VALIDATE THE USER INPUT
+#
 
-#if x == "rock": # "paper" "scissors"
-#    print("VALID")
-#else:
-#    print("OOPS, INVALID, PLEASE TRY AGAIN")
-#    exit()
+VALID_OPTIONS = ["rock", "paper", "scissors"] # this is a list
 
-if (x == "rock") or (x == "paper") or (x == "scissors"):
-    print("VALID")
-else:
-    print("OOPS, INVALID, PLEASE TRY AGAIN")
+if user_choice not in VALID_OPTIONS:
+    print("OOPS, INVALID, PLEASE TRY AGAIN!")
     exit()
 
-#print("LATER MESSAGES")
-
-print("USER CHOSE: ", x)
-
+#
 # GENERATE A COMPUTER CHOICE
 # https://docs.python.org/3/library/random.html
 # source: https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
 #
-# import random
+
+computer_choice = random.choice(VALID_OPTIONS)
+
+print("COMPUTER CHOSE:", computer_choice)
+
 #
-# foo = ['a', 'b', 'c', 'd', 'e']
-# print(random.choice(foo))
-
-# more about datatypes lists, and tuples, etc next class
-#valid_options = ("rock", "paper", "scissors") # tuple
-valid_options = ["rock", "paper", "scissors"] # list
-
-c = random.choice(valid_options)
-
-print("COMPUTER CHOSE:", c)
-
-
 # DETERMINE THE WINNER
-
-
-#print("YOU WON")
-
-#print("COMPUTER WON")
-
-# shared by Andrew via Slack
-# if x == c:
-#     print("It's a tie, try again")
-# elif x == "rock" and c == "paper":
-#     print ("You Lose! Play again")
-# elif x ==  "rock" and c == "scissors":
-#     print ("You win! Yay!")
-# elif x == "paper" and c == "rock":
-#     print ("you win! Yay!")
-# elif x == "paper" and c == "scissors":
-#     print ("You lose! Better luck next time.")
-# elif x == "scissors" and c == "rock":
-#     print ("You lose! Sorry!")
-# elif x == "scissors" and c == "paper":
-#     print ("You win! Yay!")
-
-# alternative dictionary-based approach!!!
+#
 
 winners = {
     "rock": {
@@ -115,10 +63,11 @@ winners = {
     }
 }
 
-winner = winners[x][c]
-if winner == x:
+winner = winners[user_choice][computer_choice]
+
+if winner == user_choice:
     print("YOU WON!")
-elif winner == c:
+elif winner == computer_choice:
     print("YOU LOST")
 else:
     print("TIE")
